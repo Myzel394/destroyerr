41eb81e67bbe235fda99692e27a5a78faf62274c
Myzel394 <github.7a2op@simplelogin.co>
2025-07-11 13:55:39 +0200

    feat: Add initial project

---
+++ flake.nix
@@ -0,0 +1,26 @@
+/* Run `nix develop --command $SHELL` */
+{
+  description = "Dev Shell for destroyerr";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, utils, ... } @ inputs: 
+    utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+            yarn
+            corepack
+            git
+          ];
+        };
+      }
+    );
+}
